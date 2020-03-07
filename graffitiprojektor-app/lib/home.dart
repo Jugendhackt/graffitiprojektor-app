e9cf915a065bac48178f81f2e9bbad295d076c5a
abc013 <abc013@alpenjodel.de>
2020-03-07 17:43:02 +0100

    WIP 2

--- graffitiprojektor-app/lib/home.dart
+++ graffitiprojektor-app/lib/home.dart
@@ -1,20 +1,66 @@
-import 'package:graffitiprojektor_app/screens/augmented_faces.dart';
+//import 'package:graffitiprojektor_app/screens/augmented_faces.dart';
 import 'package:flutter/material.dart';
 import 'package:graffitiprojektor_app/screens/graffitiprojektor.dart';
-import 'screens/hello_world.dart';
+/*import 'screens/hello_world.dart';
 import 'screens/custom_object.dart';
 import 'screens/runtime_materials.dart';
 import 'screens/texture_and_rotation.dart';
-import 'screens/auto_detect_plane.dart';
+import 'screens/auto_detect_plane.dart';*/
 
 class HomeScreen extends StatelessWidget {
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Graffitiprojector App'),
       ),
-      body: ListView(
+      key: _formKey,
+      body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter some text',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    // Validate will return true if the form is valid, or false if
+                    // the form is invalid.
+                    if (_formKey.currentState.validate()) {
+                      // Process data.
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ),
+              ListView(
+                children: <Widget>[
+                  ListTile(
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) => GraffitiProjektor()));
+                    },
+                    title: Text("graffiti projector"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      /*body: ListView(
         children: <Widget>[
           ListTile(
             onTap: () {
@@ -66,7 +112,7 @@ class HomeScreen extends StatelessWidget {
             title: Text("augmented faces"),
           ),
         ],
-      ),
+      ),*/
     );
   }
 }
